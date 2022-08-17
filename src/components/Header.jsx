d57d9b7b8ec457cf8aeaaafbacdd6831cf832d95
ThomasRousseau1 <thomas.rousseau500@outlook.fr>
2022-08-17 18:06:59 +0200

    Changed Link with NavLinks for active links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 // import logo from '../../public/assets/shared/logo.svg'
 import '../sass/index.scss';
 
@@ -10,10 +10,10 @@ function Header() {
                     <img src="/assets/shared/logo.svg" alt="Space Tourism" title="Space Tourism" className='header__logo'/>
             </Link>
             <div className='header__links'>
-                <Link to="/" className='header__link'><span>00</span>Home</Link>
-                <Link to="/destination" className='header__link'><span>01</span>Destination</Link>
-                <Link to="/crew" className='header__link'><span>02</span>Crew</Link>
-                <Link to="/technology" className='header__link'><span>03</span>Technology</Link>
+                <NavLink to="/" className='header__link'><span>00</span>Home</NavLink>
+                <NavLink to="/destination" className='header__link'><span>01</span>Destination</NavLink>
+                <NavLink to="/crew" className='header__link'><span>02</span>Crew</NavLink>
+                <NavLink to="/technology" className='header__link'><span>03</span>Technology</NavLink>
             </div>
         </header>
         </>
